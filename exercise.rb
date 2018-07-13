0dbe3f333cbc4393ed1a7b823e3d594c0bfdc985
Emanuele Malfara <emanuele.malfara@trovaprezzi.it>
2018-07-13 13:34:27 +0200

    * red tests

---
+++ exercise.rb
@@ -0,0 +1,9 @@
+class Exercise
+  def initialize(k)
+    @k = k
+  end
+
+  def calc_sum(*stream)
+    return []
+  end
+end
---
+++ test_exercise.rb
@@ -0,0 +1,16 @@
+require_relative "exercise"
+require "test/unit"
+
+class TestExercise < Test::Unit::TestCase
+  test 'exercise base test' do
+    assert_equal([12, 9, 2, 1], Exercise.new(24).calc_sum(12, 1, 61, 5, 9, 2))
+  end
+
+  test 'test exercise' do
+    assert_equal([12, 9, 2, 1], Exercise.new(24).calc_sum(12, 1, 61, 5, 9, 2))
+  end
+
+  test 'test exercise with no results' do
+    assert_equal([], Exercise.new(24000).calc_sum(12, 1, 61, 5, 9, 2))
+  end
+end
