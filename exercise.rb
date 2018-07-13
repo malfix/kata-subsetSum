0b514f54c7fa403ffff9ba440f31244bdf306ca3
Emanuele Malfara <emanuele.malfara@trovaprezzi.it>
2018-07-13 14:27:27 +0200

    tests greeeen

--- exercise.rb
+++ exercise.rb
@@ -4,6 +4,23 @@ class Exercise
   end
 
   def calc_sum(*stream)
+    @stream = stream.select{|el| el <= @k }.sort
+    return find_match([], 0)
+  end
+
+  def find_match(corrent_array, start_index)
+
+    if corrent_array.sum == @k
+      return corrent_array
+    end
+    if corrent_array.sum < @k
+      @stream.each_with_index do |el, index|
+        next if start_index > index # optimize O(n^2) -> O(n!)
+        temporary_solutions = corrent_array.dup << el
+        result_to_check = find_match(temporary_solutions, index + 1)
+        return result_to_check.sort if result_to_check.size > 0
+      end
+    end
     return []
   end
 end
--- test_exercise.rb
+++ test_exercise.rb
@@ -3,11 +3,11 @@ require "test/unit"
 
 class TestExercise < Test::Unit::TestCase
   test 'exercise base test' do
-    assert_equal([12, 9, 2, 1], Exercise.new(24).calc_sum(12, 1, 61, 5, 9, 2))
+    assert_equal([12, 9, 2, 1].sort, Exercise.new(24).calc_sum(12, 1, 61, 5, 9, 2))
   end
 
   test 'test exercise' do
-    assert_equal([12, 9, 2, 1], Exercise.new(24).calc_sum(12, 1, 61, 5, 9, 2))
+    assert_equal([12, 9, 2, 1].sort, Exercise.new(24).calc_sum(12, 1, 61, 5, 9, 2))
   end
 
   test 'test exercise with no results' do
